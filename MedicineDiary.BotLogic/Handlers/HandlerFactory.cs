48179751f0d37749c92c4fb43d9aceaaddf86cdf
NikM <moshkovskiynikolay@mail.ru>
2025-02-09 19:20:56 +0300

    Add first handlers to BotLogic

---
+++ MedicineDiary.BotLogic/Handlers/HandlerFactory.cs
@@ -0,0 +1,17 @@
+﻿using MedicineDiary.BotLogic.Abstractions;
+using MedicineDiary.Models.Enums;
+
+namespace MedicineDiary.BotLogic.Handlers
+{
+    internal class HandlerFactory
+    {
+        private readonly Dictionary<ChatStateEnum, IHandler> _handlers;
+        public HandlerFactory()
+        {
+            _handlers = new Dictionary<ChatStateEnum, IHandler> {
+                { ChatStateEnum.NoRegistred, new NoRegistredHandler() },
+                { ChatStateEnum.Registred, new RegistredHandler() }
+            };
+        }
+    }
+}
---
+++ MedicineDiary.BotLogic/Handlers/NoRegistredHandler.cs
@@ -0,0 +1,17 @@
+﻿using MedicineDiary.BotLogic.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicineDiary.BotLogic.Handlers
+{
+    internal class NoRegistredHandler : IHandler
+    {
+        public Task HandleAsync(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
